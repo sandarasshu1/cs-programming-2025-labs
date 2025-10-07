e94575a30b315a98c39e742c1c171a8207fdff7f
sandarasshu1 <Dimonpetrov13072019@gmail.com>
2025-10-07 14:13:09 +1100

    коммит

---
+++ labs/lab3/задание 4.py
@@ -0,0 +1,7 @@
+number = int(input("Введите число для вычисления факториала: "))
+factorial = 1
+
+for i in range(1, number + 1):
+    factorial *= i
+
+print(f"Факториал числа {number} равен {factorial}")
